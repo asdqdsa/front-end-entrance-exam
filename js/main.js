9521eb8547da1479c15c806baa7b2b813bd8b100
asdqdsa <asdqdsa@protonmail.com>
2025-07-25 20:09:34 +0400

    feat: add download pdf button, add mobile styling

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
-document.querySelectorAll('[contenteditable]').forEach((el) =>
-  el.addEventListener('input', () => {
-    el.classList.add('edit-animated');
-    setTimeout(() => el.classList.remove('edit-animated'), 300);
-  })
-);
+import html2pdf from 'html2pdf.js';
 
-const LOCAL_STORAGE_KEY = '__cv_key__';
-const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+function getHtmlToPdf() {
+  const btn = document.querySelector('#download-btn');
+  if (!btn) return;
 
-document.querySelectorAll('[contenteditable]').forEach((el, idx) => {
-  const key = `element-${idx}`;
+  btn.addEventListener('click', () => {
+    const element = document.querySelector('.cv');
+    if (!element) return;
 
-  if (savedData[key]) {
-    el.textContent = savedData[key];
-  }
-
-  el.addEventListener('input', () => {
-    savedData[key] = el.textContent;
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedData));
+    html2pdf()
+      .set({
+        margin: 0,
+        filename: 'CV.pdf',
+        image: { type: 'jpeg', quality: 1.0 },
+        html2canvas: { scale: 2, useCORS: true },
+        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+      })
+      .from(element)
+      .save();
   });
-});
+}
+
+function addAnimationOnEdit() {
+  document.querySelectorAll('[contenteditable]').forEach((el) =>
+    el.addEventListener('input', () => {
+      el.classList.add('edit-animated');
+      setTimeout(() => el.classList.remove('edit-animated'), 300);
+    })
+  );
+}
+
+function useLS() {
+  const LOCAL_STORAGE_KEY = '__cv_key__';
+  const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+
+  document.querySelectorAll('[contenteditable]').forEach((el, idx) => {
+    const key = `element-${idx}`;
+
+    if (savedData[key]) {
+      el.textContent = savedData[key];
+    }
+
+    el.addEventListener('input', () => {
+      savedData[key] = el.textContent;
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedData));
+    });
+  });
+}
+
+getHtmlToPdf();
+addAnimationOnEdit();
+useLS();
