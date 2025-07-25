4cde2150cae8224640ae66217f891f61dcf39693
asdqdsa <asdqdsa@protonmail.com>
2025-07-25 19:05:46 +0400

    feat: add edit text element on click, add LS support

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("main.js");
+document.querySelectorAll('[contenteditable]').forEach((el) =>
+  el.addEventListener('input', () => {
+    el.classList.add('edit-animated');
+    setTimeout(() => el.classList.remove('edit-animated'), 300);
+  })
+);
+
+const LOCAL_STORAGE_KEY = '__cv_key__';
+const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+
+document.querySelectorAll('[contenteditable]').forEach((el, idx) => {
+  const key = `element-${idx}`;
+
+  if (savedData[key]) {
+    el.textContent = savedData[key];
+  }
+
+  el.addEventListener('input', () => {
+    savedData[key] = el.textContent;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedData));
+  });
+});
